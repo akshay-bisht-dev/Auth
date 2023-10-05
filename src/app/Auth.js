fafbd5808e3e49d128c34148c6ffbefecadf5b45
AKSHAY BISHT <akshay.bisht.dev@gmail.com>
2023-10-05 14:30:37 +0530

    My Auth Initial Commit

---
+++ src/app/Auth.js
@@ -0,0 +1,14 @@
+"use client";
+const Auth = () => {
+  return (
+    <>
+      <div className="max-w-2xl py-48 my-4 mx-auto">
+        <h1 className="text-5xl text-center">
+          Welcome <span className="text-red-600 font-bold">Auth</span> Page
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Auth;
